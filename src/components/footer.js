dd82dc417b241bf8dac1a7bbc42e0aca117e1f53
Hugo Gomes <hugo@sketch.com>
2020-02-03 01:27:41 +0000

    flex footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -75,7 +75,7 @@ const Footer = () => {
   `)
 
   return (
-    <div>
+    <div className="flex flex-column w-100 justify-center">
       <div className="Newsletter w-100 h4 pa3 mb3">
         <h3 className="w-100 tc">
           {subscribed
@@ -99,7 +99,7 @@ const Footer = () => {
           </div>
         ) : null}
       </div>
-      <footer className="flex h4 bt b--vermelho justify-around pt2">
+      <footer className="flex flex-row-l flex-column h4-l bt b--vermelho justify-around pt2 items-center">
         <Card
           image={data.beneficiario.childImageSharp.fixed}
           alt=""
