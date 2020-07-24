c8a9b05142d7d49574f3cb54849ac0bc2064b726
Hugo Gomes <hugo@sketch.com>
2020-07-24 20:50:45 +0100

    Build

--- src/components/tabela.js
+++ src/components/tabela.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react"
 
 const TabelaEspecies = (props) => {
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(typeof Image !== 'undefined')
   const [tabela, setTabela] = useState("")
   const imgClass = "dim"
   let img = "";
+  const location = typeof window !== 'undefined' ? window.location.hash : '';
 
-  switch(window.location.hash) {
+  switch(location) {
     case '#lebrescoelhos':
       img = "https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/wilder-Lebres-e-coelhos.jpg"
       if(tabela !== img) {
@@ -49,8 +50,8 @@ const TabelaEspecies = (props) => {
         setLoading(true)
       }
   }
-  console.log('IS LOADING', isLoading)
-  if (isLoading) {
+
+  if (isLoading && typeof Image !== 'undefined') {
     const image = new Image()
     image.onload = () => setLoading(false)
     image.src = tabela;
@@ -58,12 +59,12 @@ const TabelaEspecies = (props) => {
   return (
     <div className={props.className}>
       <nav className="tabela-menu">
-        <a className={`${window.location.hash === "#insectivoros" || window.location.hash === "" ? "selected" : ""}`} c href="#insectivoros"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/ourico.png" alt="Insectivoros" /></a>
-        <a className={`${window.location.hash === "#lebrescoelhos" ? "selected" : ""}`} href="#lebrescoelhos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/lebre.png" alt="Lebres e Coelhos" /></a>
-        <a className={`${window.location.hash === "#roedores" ? "selected" : ""}`} href="#roedores"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/esquilo.png" alt="Roedores" /></a>
-        <a className={`${window.location.hash === "#morcegos" ? "selected" : ""}`} href="#morcegos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/morcego.png" alt="Morcegos" /></a>
-        <a className={`${window.location.hash === "#baleiasgolfinhos" ? "selected" : ""}`} href="#baleiasgolfinhos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/baleia.png" alt="Baleias, Golfinhos" /></a>
-        <a className={`${window.location.hash === "#carnivorosfocas" ? "selected" : ""}`} href="#carnivorosfocas"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/lobo.png" alt="Carnívoros, Focas" /></a>
+        <a className={`${location === "#insectivoros" || location === "" ? "selected" : ""}`} c href="#insectivoros"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/ourico.png" alt="Insectivoros" /></a>
+        <a className={`${location === "#lebrescoelhos" ? "selected" : ""}`} href="#lebrescoelhos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/lebre.png" alt="Lebres e Coelhos" /></a>
+        <a className={`${location === "#roedores" ? "selected" : ""}`} href="#roedores"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/esquilo.png" alt="Roedores" /></a>
+        <a className={`${location === "#morcegos" ? "selected" : ""}`} href="#morcegos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/morcego.png" alt="Morcegos" /></a>
+        <a className={`${location === "#baleiasgolfinhos" ? "selected" : ""}`} href="#baleiasgolfinhos"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/baleia.png" alt="Baleias, Golfinhos" /></a>
+        <a className={`${location === "#carnivorosfocas" ? "selected" : ""}`} href="#carnivorosfocas"><img className={imgClass} src="https://www.livrovermelhodosmamiferos.pt/wordpress/wp-content/uploads/2020/07/lobo.png" alt="Carnívoros, Focas" /></a>
       </nav>
       <img className={`${isLoading ? "tabela-loading" : ""}`} src={tabela} alt="Tabela dos estatutos" />
     </div>
